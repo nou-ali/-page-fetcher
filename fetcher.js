074dc5846940820594e4cf84039f6530231ae81c
nou-ali <noura4403@gmail.com>
2021-12-03 03:12:01 +0000

    fixed bugs and linted

--- fetcher.js
+++ fetcher.js
@@ -1,8 +1,8 @@
-// Install and use the request library to make the HTTP request 
+// Install and use the request library to make the HTTP request
 // Use Node's fs (file system) module to write the file
 // Use the callback based approach we've been learning so far
 // Do not use the pipe function
-// Do not use synchronous functions 
+// Do not use synchronous functions
 const request = require('request'); //Request is designed to be the simplest way possible to make http calls.
 const fs = require('fs'); //The Node. js file system module allows you to work with the file system on your computer.
 
@@ -13,8 +13,7 @@ request('http://www.example.edu', (error, response, body) => {
   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   // console.log('body:', body); // Print the HTML for the example.edu homepage.
 
-  const content = "some content!";
-  fs.writeFile('/vagrant/w2/d3/page-fetcher/index.html', content, error => {
+  fs.writeFile('/vagrant/w2/d3/page-fetcher/index.html', body, error => {
     if (error) {
       console.log(error);
       return;
@@ -26,6 +25,8 @@ request('http://www.example.edu', (error, response, body) => {
 
   });
 });
+
+
 
 
 
